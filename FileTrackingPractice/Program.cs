ae3bc752fba195be1a648bda2f20caa5912c8981
mismetdemir <mismetdemir@hotmail.com>
2026-07-29 13:19:43 +0300

    Config: Added SQLite connection configuration

--- FileTrackingPractice/Program.cs
+++ FileTrackingPractice/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using FileTrackingPractice.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
