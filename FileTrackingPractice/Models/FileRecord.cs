912f62f3e7d0d10cc40567e8da913c551ab05a87
mismetdemir <mismetdemir@hotmail.com>
2026-07-29 11:53:33 +0300

    Add: FileRecord model added

---
+++ FileTrackingPractice/Models/FileRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace FileTrackingPractice.Models
+{
+    public class FileRecord
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Extension { get; set; } = string.Empty;
+        public long Size { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModifiedAt { get; set; }
+    }
+}
