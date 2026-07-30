1f9314d3ff7dfb9a9426ff4616fe16f42708b743
mismetdemir <mismetdemir@hotmail.com>
2026-07-30 09:37:07 +0300

    Config: Added file scan settings

---
+++ FileTrackingPractice/Config/FileScanSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace FileTrackingPractice.Config
+{
+    public class FileScanSettings
+    {
+        public string FolderPath { get; set; } = string.Empty;
+        public int IntervalInSeconds { get; set; } 
+    }
+}
