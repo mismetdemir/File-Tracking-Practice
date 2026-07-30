a13d772a51bc2c4933308ed1d57e5a5a65456eae
mismetdemir <mismetdemir@hotmail.com>
2026-07-30 09:49:29 +0300

    Add: Created DTO for scan results

---
+++ FileTrackingPractice/DTOs/ScanResultDto.cs
@@ -0,0 +1,12 @@
+﻿namespace FileTrackingPractice.DTOs
+{
+    public class ScanResultDto
+    {
+        public int FilesFound { get; set; }
+        public int FilesAdded { get; set; }
+        public int FilesSkipped { get; set; }
+        public int FilesFailed { get; set; }
+        public DateTime ScanStartedAt { get; set; }
+        public DateTime ScanFinishedAt { get; set; }
+    }
+}
