2e48765f953b455d8e15a455bf606c0b67d8400d
mismetdemir <mismetdemir@hotmail.com>
2026-07-30 09:43:08 +0300

    Config: Connected file scan setting in JSON to the settings class

--- FileTrackingPractice/Program.cs
+++ FileTrackingPractice/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using FileTrackingPractice.Data;
+using FileTrackingPractice.Config;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,9 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<FileScanSettings>(
+    builder.Configuration.GetSection("FileScan"));
 
 var app = builder.Build();
 
