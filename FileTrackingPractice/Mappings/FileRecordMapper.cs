3e1b673d9c74cdc26856688c3b9a8d5f9fd38596
mismetdemir <mismetdemir@hotmail.com>
2026-07-31 12:41:41 +0300

    Add: Added DTO Mapper for use in endpoints

---
+++ FileTrackingPractice/Mappings/FileRecordMapper.cs
@@ -0,0 +1,24 @@
+﻿using FileTrackingPractice.DTOs;
+using FileTrackingPractice.Models;
+
+namespace FileTrackingPractice.Mappings
+{
+    public class FileRecordMapper
+    {
+        public FileRecordDto MapToDto(FileRecord fileRecord, string rootFolderPath)
+        {
+            var rootPath = Path.GetFullPath(rootFolderPath);
+
+            return new FileRecordDto
+            {
+                Id = fileRecord.Id,
+                Name = fileRecord.Name,
+                Extension = fileRecord.Extension,
+                Size = fileRecord.Size,
+                CreatedAt = fileRecord.CreatedAt,
+                LastModifiedAt = fileRecord.LastModifiedAt,
+                RelativePath = Path.GetRelativePath(rootPath, fileRecord.Path)
+            };
+        }
+    }
+}
