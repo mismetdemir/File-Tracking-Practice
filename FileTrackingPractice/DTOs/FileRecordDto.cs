2133afd14345dd19c3f52b63a63dec632013c1a8
mismetdemir <mismetdemir@hotmail.com>
2026-07-31 11:56:49 +0300

    Add: Created file record DTO for API calls

---
+++ FileTrackingPractice/DTOs/FileRecordDto.cs
@@ -0,0 +1,13 @@
+﻿namespace FileTrackingPractice.DTOs
+{
+    public class FileRecordDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Extension { get; set; } = string.Empty;
+        public long Size { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModifiedAt { get; set; }
+        public string RelativePath { get; set; } = string.Empty;
+    }
+}
