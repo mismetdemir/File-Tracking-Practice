ffce70ffe2c072c0e5d1232bb96676a0f47bc313
mismetdemir <mismetdemir@hotmail.com>
2026-08-11 14:01:41 +0300

    Update: Updated the database with new migration

---
+++ FileTrackingPractice/Migrations/20260811105546_AddHashToFileRecord.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileTrackingPractice.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHashToFileRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Hash",
+                table: "FileRecords",
+                type: "TEXT",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hash",
+                table: "FileRecords");
+        }
+    }
+}
